aa1b20334f61b776150f7ea06c87fb246215a018
Wubante Tilahun <wubante123@gmail.com>
2026-08-29 11:14:19 +0300

    Fix SQLite CONN_MAX_AGE for instant PythonAnywhere reload

--- jobs/settings.py
+++ jobs/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        "CONN_MAX_AGE": 300,
+        "CONN_MAX_AGE": 0,
     }
 }
 
