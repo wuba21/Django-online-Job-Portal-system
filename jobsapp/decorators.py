ac3d534131379e7ab9ff35fc8d9c130891830ba9
Wubante Tilahun <wubante123@gmail.com>
2026-09-02 10:57:54 +0300

    Fix 403 Forbidden: allow superusers/staff to bypass employee/employer role decorators

--- jobsapp/decorators.py
+++ jobsapp/decorators.py
@@ -1,25 +1,35 @@
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
 
 
 def user_is_employer(function):
     def wrap(request, *args, **kwargs):
         if not request.user.is_authenticated:
-            raise PermissionDenied
+            return redirect("accounts:login")
+        # Superusers/staff bypass role checks
+        if request.user.is_superuser or request.user.is_staff:
+            return function(request, *args, **kwargs)
         if request.user.role == "employer":
             return function(request, *args, **kwargs)
-        else:
-            raise PermissionDenied
+        raise PermissionDenied
 
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
     return wrap
 
 
 def user_is_employee(function):
     def wrap(request, *args, **kwargs):
         if not request.user.is_authenticated:
-            raise PermissionDenied
+            return redirect("accounts:login")
+        # Superusers/staff bypass role checks
+        if request.user.is_superuser or request.user.is_staff:
+            return function(request, *args, **kwargs)
         if request.user.role == "employee":
             return function(request, *args, **kwargs)
-        else:
-            raise PermissionDenied
+        raise PermissionDenied
 
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
     return wrap
