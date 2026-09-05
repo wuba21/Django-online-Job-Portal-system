74caaf10f0e22071bba4c176732fb9374daece68
Wubante Tilahun <wubante123@gmail.com>
2026-09-05 12:02:16 +0300

    Fix migration conflict: add 0022 merge migration for 0020_add_slug and 0021_merge

---
+++ jobsapp/migrations/0022_merge_slug_and_existing.py
@@ -0,0 +1,15 @@
+# Generated merge migration to resolve conflict between
+# 0020_add_slug_to_job and 0021_merge_20260902_0855
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('jobsapp', '0020_add_slug_to_job'),
+        ('jobsapp', '0021_merge_20260902_0855'),
+    ]
+
+    operations = [
+    ]
